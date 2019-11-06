2373e7ef3856da7ecaabb8d74aea5e3c342c0ba1
Sarah Reichelt <sarah@troz.net>
2019-11-06 15:53:47 +1000

    Initial Commit

---
+++ SwiftUI-Mac/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-Mac
+//
+//  Created by Sarah Reichelt on 6/11/19.
+//  Copyright © 2019 Sarah Reichelt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
